c0656836e2ef0b1c2ff6d8b68d3d11ac7973282e
Nick Friday <nfriday@yandex.ru>
2022-11-18 14:14:41 +0300

    feat: toggle box & opts w/ setup

--- README.md
+++ README.md
@@ -21,6 +21,20 @@ use({
 })
 ```
 
+## Setup
+
+You **don't need** to execute a setup function to use the defaults but in case you
+want to redefine them, you can do it with `setup`:
+
+```lua
+require('markdown-togglecheck').setup({
+  -- create empty checkbox on item without any while toggling
+  create = true,
+  -- remove checked checkbox instead of unckecking it while toggling
+  remove = false,
+})
+```
+
 ## Usage
 
 The plugin does not provide default keybindings.
@@ -28,7 +42,10 @@ The plugin does not provide default keybindings.
 You should define one for `require('markdown-togglecheck').toggle()`:
 
 ```lua
+-- toggle checked / create checkbox if it doesn't exist
 vim.api.nvim_set_keymap('n', '<leader>nn', require('markdown-togglecheck').toggle)
+-- toggle checkbox (it doesn't remember toggle state and always creates [ ])
+vim.api.nvim_set_keymap('n', '<leader>nN', require('markdown-togglecheck').toggle_box)
 ```
 
 You also might add some highlight queries yourself for the check boxes. For
--- lua/markdown-togglecheck.lua
+++ lua/markdown-togglecheck.lua
@@ -3,9 +3,48 @@ local utils = require('treesitter-utils')
 
 local M = {}
 
+---@class ToggleCheckOptions
+---@field create boolean
+---@field remove boolean
+
+---@type ToggleCheckOptions
+M.opts = {
+  create = true,
+  remove = false,
+}
+
+---@param opts ToggleCheckOptions
+M.setup = function(opts)
+  M.opts = vim.tbl_deep_extend('force', M.opts, opts or {})
+end
+
+M.toggle_box = function()
+  local node = ts.get_node_at_cursor(nil, true)
+  local item = utils.find_parent_node(node, 'list_item')
+  if (not item) then
+    return
+  end
+
+  local unchecked = utils.find_child_node(item, 'task_list_marker_unchecked')
+  local checked = utils.find_child_node(item, 'task_list_marker_checked')
+  local box = checked or unchecked
+
+  if box then
+    utils.set_node_text(box, {})
+    local marker = item:child()
+    utils.set_node_text(marker, utils.get_node_text(marker):sub(1, 1))
+    return
+  end
+
+  local content = utils.find_child_node(item, 'paragraph')
+  if not content then return end
+  local text = utils.get_node_text(content)
+
+  text[1] = '[ ] ' .. text[1]
+  utils.set_node_text(content, text)
+end
+
 M.toggle = function()
-  ---@type TSNode
-  ---@diagnostic disable-next-line: assign-type-mismatch
   local node = ts.get_node_at_cursor(nil, true)
   local item = utils.find_parent_node(node, 'list_item')
   if (not item) then
@@ -20,8 +59,16 @@ M.toggle = function()
 
   local checked = utils.find_child_node(item, 'task_list_marker_checked')
   if checked then
-    utils.set_node_text(checked, '[ ]')
+    if M.opts.remove then
+      M.toggle_box()
+    else
+      utils.set_node_text(checked, '[ ]')
+    end
     return
+  end
+
+  if M.opts.create then
+    M.toggle_box()
   end
 end
 
